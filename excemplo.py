d2dfb9591ee671bee39089525b78fb8c74d452d9
Arthur Lôbo <arthurlobo.edu17@hotmail.com>
2025-02-22 19:34:37 -0300

    Atualizado e pronto pra tacar o terror

--- excemplo.py
+++ excemplo.py
@@ -6,17 +6,16 @@ num_aleatorio1 = int(input())
 roleta = random.randint(1,1)
 
 texto = [
-    echo set WshShell = WScript.CreateObject("WScript.Shell") > %tmp%\tmp.vbs
-    echo WScript.Quit (WshShell.Popup( "You have 10 seconds to Click 'OK'." ,10 ,"Click OK", 0)) >> %tmp%\tmp.vbs
-    cscript /nologo %tmp%\tmp.vbs
-    if %errorlevel%==1 (
-     echo You Clicked OK
-    ) else (
-     echo The Message timed out.
-    )
-    del %tmp%\tmp.vbs
+    "echo set WshShell = WScript.CreateObject(\"WScript.Shell\") > %tmp%//tmp.vbs",
+        "echo WScript.Quit (WshShell.Popup( \"SEU COMPUTADOR ESTA INFESTADO .\" ,10 ,\"Click OK\", 0)) >> %tmp%//tmp.vbs",
+        "cscript /nologo %tmp%//tmp.vbs",
+        "if %errorlevel%==1 (",
+        " echo You Clicked OK",
+        ") else (",
+        "echo The Message timed out.",
+        ")",
+        "del %tmp%//tmp.vbs",
 ]
-
 
 if num_aleatorio1 == roleta :
     print("Seu arquivo mais recente foi deletado, parabéns!!!")
